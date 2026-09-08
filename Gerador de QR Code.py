e3de58494d15b919e047fcdc7ddab4af19ed488a
Silas Lopes de Souza <silaslopesdesouza@gmail.com>
2026-09-08 10:46:08 -0300

    Refactor comments in QR Code generator script
    Updated comments for clarity and consistency.

--- Gerador de QR Code.py
+++ Gerador de QR Code.py
@@ -1,21 +1,21 @@
 import qrcode
-# 1. Defina o link do seu QR Code
+# 1. link do seu QR Code
 link = "https://missceliaassistentevirtual.lovable.app/"
 
-# 2. Configure o design e tamanho do QR Code
+# 2. Design e tamanho do QR Code
 qr = qrcode.QRCode(
     version=1,       # Define a complexidade/tamanho (1 a 40)
     box_size=10,     # Tamanho de cada quadrado do código (pixels)
     border=4,        # Espessura da margem branca
 )
 
-# 3. Adicione o link e compile
+# 3. link e compile
 qr.add_data(link)
 qr.make(fit=True)
 
-# 4. Gere a imagem (você pode mudar as cores aqui)
+# 4. Gere a image
 imagem = qr.make_image(fill_color="black", back_color="white")
 
-# 5. Salve o arquivocls
+# 5. Arquivo fica salvo geralmente na mesma pasta em que o arquivo original está
 imagem.save("misscelia_qrcode.png")
 print("QR Code gerado com sucesso!")
